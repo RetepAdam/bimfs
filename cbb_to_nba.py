27c0abc8b13a229bd906d6998e66beb20d3a11fa
RetepAdam <peter.a.nygaard@gmail.com>
2017-06-08 15:27:43 -0600

    housekeeping

--- cbb_to_nba.py
+++ cbb_to_nba.py
@@ -3,22 +3,30 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 import statsmodels.api as sm
+from sklearn.linear_model import LinearRegression
 
 df_nba = pd.read_csv('data/rookies_from_cbb.csv')
 df_cbb = pd.read_csv('data/cbb_to_rookies.csv')
 
+df_nba = df_nba[df_nba['MP'] >= 100]
+df_cbb = df_cbb[df_cbb['Player'].isin(df_nba['Player'])]
+
 X = np.array(df_cbb[df_cbb.columns[2:]])
 cols = df_nba.columns[25:]
 for col in cols:
+    print(col)
     y = np.array(df_nba[col])
-
+#
     X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=21)
-
+#
     rf = RandomForestRegressor(random_state=66, max_depth=5)
     rf.fit(X_train, y_train)
-    print(col, rf.score(X_test, y_test))
-
-# model_X = sm.add_constant(X)
-# model = sm.OLS(y, model_X)
-# results = model.fit()
-# print(results.summary())
+    regr = LinearRegression()
+    regr.fit(X_train, y_train)
+    model_X = sm.add_constant(X)
+    model = sm.OLS(y, model_X)
+    results = model.fit()
+    print('RF: {0}'.format(rf.score(X_test, y_test)))
+    print('Linear: {0}'.format(regr.score(X_test, y_test)))
+    print('OLS R2: {0}'.format(results.rsquared_adj))
+    print('')
--- dleague_to_nba_comp.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE nba_totals
-(player TEXT, pos TEXT, age FLOAT, team TEXT, g INT, gs INT, mp FLOAT, m_fg INT, a_fg INT, p_fg FLOAT, m_3p INT, a_3p INT, p_3p FLOAT, m_2p INT, a_2p INT, p_2p FLOAT, p_efg FLOAT, ft_m INT, ft_a INT, ft_p FLOAT, orb INT, drb INT, trb INT, ast INT, stl INT, blk INT, tov INT, pf INT, pts INT, yr INT);
-
-COPY nba_totals FROM '/Users/Peter/desktop/galvanize/bimfs/data/player_totals.csv' DELIMITER ',' CSV HEADER;
-
-CREATE TABLE dl_totals
-(player TEXT, team TEXT, age FLOAT, g INT, gs INT, mp FLOAT, m_fg INT, a_fg INT, p_fg FLOAT, m_3p INT, a_3p INT, p_3p FLOAT, m_2p INT, a_2p INT, p_2p FLOAT, p_efg FLOAT, ft_m INT, ft_a INT, ft_p FLOAT, orb INT, drb INT, trb INT, ast INT, stl INT, blk INT, tov INT, pf INT, pts INT, yr INT);
-
-COPY dl_totals FROM '/Users/Peter/desktop/galvanize/bimfs/data/player_NBADL_totals.csv' DELIMITER ',' CSV HEADER;
