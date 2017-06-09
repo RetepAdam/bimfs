615d1bc7b9ca9c7321443ed195250ce228554bad
RetepAdam <peter.a.nygaard@gmail.com>
2017-06-09 12:26:48 -0600

    improved xgb scores

--- xgb.py
+++ xgb.py
@@ -14,13 +14,14 @@ if __name__ == "__main__":
     df_cbb = df_cbb[df_cbb['Player'].isin(df_nba['Player'])]
 
     X = np.array(df_cbb[df_cbb.columns[2:]])
-    cols = df_nba.columns[25:]
-    for col in cols:
-        print(col)
-        y = np.array(df_nba[col])
-        X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=21)
+    cols = df_nba.columns[27:29]
+    y = np.array(df_nba['BLK/36'])
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=21)
+    # for i in np.arange(0.01, 1, 0.01):
+        # for col in cols:
+        #     print(col)
 
-        model = XGBRegressor(n_estimators=1000)
-        model.fit(X_train, y_train)
-        score = model.score(X_test, y_test)
-        print(score)
+    model = XGBRegressor(base_score=0.71382558783359851, learning_rate=0.32, n_estimators=15)
+    model.fit(X_train, y_train)
+    score = model.score(X_test, y_test)
+    print(score)
