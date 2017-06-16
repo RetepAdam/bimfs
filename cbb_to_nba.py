5c59afe514648afca8e79745701475e0e8275322
RetepAdam <peter.a.nygaard@gmail.com>
2017-06-16 00:42:37 -0600

    Hooplicator is officially working on a single-stat basis.

--- cbb_to_nba.py
+++ cbb_to_nba.py
@@ -11,45 +11,51 @@ from xgboost import XGBRegressor
 
 df_nba = pd.read_csv('data/rookies_from_cbb2.csv')
 df_cbb = pd.read_csv('data/cbb_to_rookies2.csv')
+df_comp = pd.read_csv('data/cbb_comp_list.csv')
 
-df_cbb['Age'] = df_nba['Age']
+# df_cbb['Age'] = df_nba['Age'] # can't grab age for new class yet
 
 df_nba = df_nba[df_nba['MP'] >= 500]
 df_cbb = df_cbb[df_cbb['Player'].isin(df_nba['Player'])]
 
+df_comp = df_comp[df_comp['MP'] >= 500]
+df_comp.reset_index(drop=True, inplace=True)
+nba_X_comp = np.array(df_comp[df_comp.columns[5:]])
+
 X = np.array(df_cbb[df_cbb.columns[1:]])
-cols = df_nba.columns[25:]
+cols = df_nba.columns[48:]
+
+i = 0
 
 # fig, axes = plt.subplots(1,3, figsize=(12,4))
 
 # for col, ax in zip(cols, axes.flatten()):
-y = np.array(df_nba['FG%'])
+for col in cols:
+    print(col)
+    y = np.array(df_nba[col])
 
-nba_X_train, nba_X_test, nba_y_train, nba_y_test = xval.train_test_split(X, y, random_state=21)
+    nba_X_train, nba_X_test, nba_y_train, nba_y_test = xval.train_test_split(X, y, random_state=21)
 
-# for i in range(1,20):
-n_trees = 100
-nba_forest = RandomForestRegressor(n_estimators=n_trees, random_state=66)
-nba_forest.fit(nba_X_train, nba_y_train)
-nba_y_hat = nba_forest.predict(nba_X_test)
-# regr = LinearRegression()
-# regr.fit(X_train, y_train)
-# model_X = sm.add_constant(X)
-# model = sm.OLS(y, model_X)
-# results = model.fit()
-print('RF: {0}'.format(nba_forest.score(nba_X_test, nba_y_test)))
-# print('Linear: {0}'.format(regr.score(X_test, y_test)))
-# print('OLS R2: {0}'.format(results.rsquared_adj))
-print('')
+    # for i in range(1,20):
+    nba_forest = RandomForestRegressor(n_estimators=1000, random_state=66)
+    nba_forest.fit(nba_X_train, nba_y_train)
+    print('RF: {0}'.format(nba_forest.score(nba_X_test, nba_y_test)))
 
-nba_inbag = fci.calc_inbag(nba_X_train.shape[0], nba_forest)
-nba_V_IJ_unbiased = fci.random_forest_error(nba_forest, nba_X_train,
-                                            nba_X_test)
+    nba_y_hat = nba_forest.predict(nba_X_comp)
 
-#     # Plot error bars for predicted MPG using unbiased variance
-#     ax.errorbar(nba_y_test, nba_y_hat, yerr=np.sqrt(nba_V_IJ_unbiased), fmt='o')
-#     ax.set_xlabel('Actual %s' % col)
-#     ax.set_ylabel('Predicted %s' % col)
+    nba_inbag = fci.calc_inbag(nba_X_train.shape[0], nba_forest)
+    nba_V_IJ_unbiased = fci.random_forest_error(nba_forest, nba_X_train,
+                                                nba_X_comp)
+
+    nba_yerr = np.sqrt(nba_V_IJ_unbiased)
+    #
+    # np.save('numpy/{0}_preds.npy'.format(i, col), nba_y_hat)
+    # np.save('numpy/{0}_yerr.npy'.format(i, col), nba_yerr)
+    # i += 1
+
+# # Plot error bars for predicted MPG using unbiased variance
+# plt.errorbar(np.arange(len(nba_y_hat)), nba_y_hat, yerr=np.sqrt(nba_V_IJ_unbiased), fmt='o')
+# plt.xlabel('Actual 3P%')
+# plt.ylabel('Predicted 3P%')
 #
-# plt.tight_layout()
 # plt.show()
--- inputs.py
+++ inputs.py
@@ -1,7 +1,29 @@
 import pandas as pd
 import numpy as np
+import scipy.stats as scs
+import matplotlib.pyplot as plt
 
 df_inputs = pd.read_csv('data/nba_inputs.csv')
+df_comp = pd.read_csv('data/cbb_comp_list.csv')
+
+df_comp = df_comp[df_comp['MP'] >= 500]
+df_comp.reset_index(drop=True, inplace=True)
 
 selected = raw_input('Enter a player name: ')
 stats = np.array(df_inputs[df_inputs['Player'] == selected])[0][2:]
+
+probabilities = []
+
+for i in range(len(stats)):
+    comp_pred = np.load('numpy/{0}_preds.npy'.format(i))
+    comp_err = np.load('numpy/{0}_yerr.npy'.format(i))
+    comp_proba = scs.norm.cdf((comp_pred - stats[i]) / (.5 * comp_err))
+    probabilities.append(comp_proba)
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+
+ax.scatter(range(len(df_comp)), probabilities[21])
+ax.set_xlabel('Index')
+ax.set_ylabel('Probability of replicating {0}'.format(df_inputs.columns[23]))
+plt.show()
